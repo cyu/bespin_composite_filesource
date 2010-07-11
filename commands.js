367bd8afcbea552a8957039e5ab5c865343146cb
Calvin Yu <csyu77@gmail.com>
2010-07-11 11:38:33 -0400

    remove env argument

--- commands.js
+++ commands.js
@@ -1,4 +1,4 @@
-exports.mount = function(env, args, request) {
+exports.mount = function(args, request) {
   var ServerFileSource = require('composite_filesource:server').ServerFileSource;
   env.files.source.addSource(new ServerFileSource(args.server));
   env.files.invalidate();
